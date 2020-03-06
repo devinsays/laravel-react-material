3d2135937ed8c32791c275d9e0492f93bdfc783a
Devin Price <devin.price@nanoglobal.com>
2020-03-06 15:32:31 -0600

    More styling updates.

--- src/Base.js
+++ src/Base.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Box, Container } from '@material-ui/core';
+import { Box, Container, Grid } from '@material-ui/core';
 
 import Header from './components/Header';
 
 const Base = ({ children }) => (
-  <>
-    <Header />
-    <Container maxWidth="lg">
-      <Box px={2} py={4}>
-        {children}
-      </Box>
-    </Container>
-  </>
+  <Grid container direction="column" style={{ minHeight: '100%' }}>
+    <Grid item>
+      <Header />
+    </Grid>
+    <Grid
+      container
+      justify="center"
+      alignItems="center"
+      style={{
+        flexGrow: '1'
+      }}
+    >
+      <Container maxWidth="lg">
+        <Box px={2} py={4}>
+          {children}
+        </Box>
+      </Container>
+    </Grid>
+  </Grid>
 );
 
 const mapStateToProps = state => ({
