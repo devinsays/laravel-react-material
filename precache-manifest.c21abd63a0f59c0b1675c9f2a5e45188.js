fd5aab4b944516ed033d581febe89b4e8e7dee34
Devin Price <devin.price@nanoglobal.com>
2020-03-11 10:43:14 -0500

    Updates

---
+++ precache-manifest.c21abd63a0f59c0b1675c9f2a5e45188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2586676fe40caad18525ab9acd719bf",
+    "url": "/laravel-react-material/index.html"
+  },
+  {
+    "revision": "fca6b25a64c2392bbb92",
+    "url": "/laravel-react-material/static/css/main.1c4c35a3.chunk.css"
+  },
+  {
+    "revision": "637d9f09524b83bdaf66",
+    "url": "/laravel-react-material/static/js/2.15f3de1d.chunk.js"
+  },
+  {
+    "revision": "a524ed73522d5e6f1539de7cd655bf05",
+    "url": "/laravel-react-material/static/js/2.15f3de1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fca6b25a64c2392bbb92",
+    "url": "/laravel-react-material/static/js/main.9e789da3.chunk.js"
+  },
+  {
+    "revision": "1c4ad7fa6512761f579b",
+    "url": "/laravel-react-material/static/js/runtime-main.327fcc56.js"
+  }
+]);
